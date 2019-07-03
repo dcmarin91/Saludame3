0ecd0236400ac93d79b54739cddb2b61c9fd09d4
dcmarin91 <dcmarin91@hotmail.com>
2019-07-03 12:08:03 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+app.use(express.urlencoded());
+
+app.get('/',(req,res) =>{
+    res.render('index');
+})
+
+app.post('/',(req,res)=>{
+    let name = req.body.name;
+    res.send(`<h1> Hola ${name}!</h1>`)
+})
+
+
+app.listen(3000,()=> console.log("Listening on port 3000"))
